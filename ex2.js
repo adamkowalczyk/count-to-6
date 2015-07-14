ef7ceddf4d934a218d697af1e4264b28fc96365e
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-07-14 11:59:23 +0100

    notes

--- ex2.js
+++ ex2.js
@@ -1,3 +1,7 @@
 var name = process.argv[2];
 
 console.log(`Hello, ${name}!\nYour name lowercased is "${name.toLowerCase()}".`);
+
+// Notes:
+
+// Template strings: in backticks ``  let you do string interpolation (like Ruby :D ) and multiline strings
